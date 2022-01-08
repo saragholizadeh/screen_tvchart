812ea4fd44c426c97fe0d2a69d762376ba173a75
saragholizadeh <saragholizadeh@gmail.com>
2022-01-08 17:01:28 +0330

    handlebars

--- dynamicChartswithHistory/screen.js
+++ dynamicChartswithHistory/screen.js
@@ -1,27 +1,61 @@
 const nodeHtmlToImage = require("node-html-to-image");
 var express = require("express");
 var app = express();
-const ejs = require("ejs");
+// const ejs = require("ejs");
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
 const fs = require("fs");
 const path = require("path");
+const internal = require("stream");
 
-const file = ejs.render("./index.ejs" , );
+// const file = ejs.render("./index.ejs");
 
-let htmlFile = fs.readFileSync(
-  path.resolve(__dirname, "./index.ejs"),
-  "utf8"
-);
+let htmlFile = fs.readFileSync(path.resolve(__dirname, "./chart.html"), "utf8");
 
 const convert = () => {
   setTimeout(() => {
-     nodeHtmlToImage({
-       output: "./Chart.png",
-       html: htmlFile,
-     }).then(() => console.log("The image was created successfully!"));
+    nodeHtmlToImage({
+      output: "./Chart.png",
+      html: htmlFile,
+      content: {
+        symbol: "EGLDUSDT",
+        interval: "1m",
+      },
+    }).then(() => console.log("The image was created successfully!"));
   }, 5000);
 };
 convert();
+
+// const nodeHtmlToImage = require("node-html-to-image");
+// var express = require("express");
+// var app = express();
+// const ejs = require("ejs");
+
+// // set the view engine to ejs
+// app.set("view engine", "ejs");
+
+// const fs = require("fs");
+// const path = require("path");
+
+// let htmlFile = fs.readFileSync(
+//   path.resolve(__dirname, "./index.ejs"),
+//   "utf8"
+// );
+
+// const convert = () => {
+//   setTimeout(() => {
+//      nodeHtmlToImage({
+//        output: "./Chart.png",
+//        html: htmlFile,
+//      }).then(() => console.log("The image was created successfully!"));
+//   }, 5000);
+// };
+// convert();
+
+// app.get('/' , (req , res )=>{
+//   res.render("index" , {symbol : "EGLDSUDT"})
+// })
+
+// app.listen(8000)
